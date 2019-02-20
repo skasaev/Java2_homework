f3ea797f938fed0cafea2bcc9b054272ba85ede7
Sanal Kasaev <ksvsnl@gmail.com>
2019-02-20 23:20:30 +0300

    add homework test 3: calculator move to method and add method for get string with max length in string array

--- src/main/java/Base.java
+++ src/main/java/Base.java
@@ -2,17 +2,36 @@ import java.util.Locale;
 import java.util.Scanner;
 
 /**
- * Addition, subtraction, multiplication and division of 2 numbers without input validation
+ * Homework test #3
+ * run the simple calculator or search for the string with maximum length in an array of a given size
  *
  * @author Sanal Kasaev
  */
 
 public class Base {
     public static void main(String[] args) {
-        System.out.println("Calculator 0.1");
+        System.out.println("Homework test #3");
 
         Scanner scanner = new Scanner(System.in).useLocale(Locale.ENGLISH);
 
+        System.out.println("Enter number of task: (1 - calculator, 2 - string array)");
+        String taskNumber = scanner.next();
+        if (taskNumber.equals("1")) {
+            System.out.println("Running simple calculator");
+            calculator(scanner);
+        } else {
+            System.out.println("Running search string with max length in array");
+            printStringWithMaxLength(scanner);
+        }
+        scanner.close();
+    }
+
+    /**
+     * Print result of 2 numbers addition, subtraction, multiplication or division without input validation
+     *
+     * @param scanner using for read system console input stream
+     */
+    private static void calculator(Scanner scanner) {
         System.out.println("Enter the first number (double): ");
         double firstNumber = scanner.nextDouble();
         System.out.println("Enter the second number (double): ");
@@ -44,7 +63,31 @@ public class Base {
                 System.exit(0);
         }
         System.out.printf(Locale.ENGLISH,"Result of operation %f %s %f = %.4f", firstNumber, symbol, secondNumber, result);
+    }
 
-        scanner.close();
+    /**
+     * Print string with maximal length in a array of a given size
+     *
+     * @param scanner using for read system console input stream
+     */
+    private static void printStringWithMaxLength(Scanner scanner) {
+        System.out.println("Enter the strings size: ");
+        int arraySize = scanner.nextInt();
+        String[] strings = new String[arraySize];
+
+        for (int i=0; i<arraySize; i++) {
+            System.out.println("Enter " + (i+1) + " element of string array:");
+            strings[i] = scanner.next();
+        }
+
+        String maxLengthString = "";
+        for (String element : strings) {
+            if (element.length() > maxLengthString.length()) {
+                maxLengthString = element;
+            }
+        }
+
+        System.out.println("String with maximal length is " + maxLengthString
+                + "\nIts length = " + maxLengthString.length());
     }
 }
